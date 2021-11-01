933c2a8b792c62ef42287432b6535419fb80d948
Aditya Mittal <44825213+aditheawesome@users.noreply.github.com>
2021-11-01 15:12:14 +0000

    added test input

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ app = Flask(__name__)
 @app.route("/")
 def main():
   return render_template("main.html")
+@app.route("/test", methods = "POST", "GET")
+def test():
+  if request.method = "POST":
+    hi = request.form["sss"]
+    return render_template("test.html", hi = hi)
 
 if __name__ == "__main__":
   app.run(debug = True)
